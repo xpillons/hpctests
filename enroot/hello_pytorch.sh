7d61241e84d0ac69a2aa5448a75fab14953e6645
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-05-06 18:24:32 +0200

    use gpu partition

--- enroot/hello_pytorch.sh
+++ enroot/hello_pytorch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sbatch -N 1 -p hpc --wait hello_pytorch.sh
+# sbatch -N 1 -p gpu --wait hello_pytorch.sh
 #SBATCH --job-name=hello_pytorch
 #SBATCH -o %x_%j.log
 #SBATCH --container-image nvcr.io\#nvidia/pytorch:21.12-py3
