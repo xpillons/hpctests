5b0772d48cb65d2b6ce189ea911ed24c8c7e43ca
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-06-18 11:14:59 +0200

    add lora model

--- diffusers/readme.md
+++ diffusers/readme.md
@@ -1,4 +1,4 @@
 Scripts and example to run text_to_image from https://github.com/huggingface/diffusers
 
 - From the login node, run ./install.sh to setup the environment in your home directory
-- sbatch test_to_image.slurm
+- to submit a job on a single GPU node run `submit.sh`
---
+++ diffusers/submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NODEDEF=$(grep "Nodename=ccsw-gpu-" /sched/ccsw/azure.conf | xargs)
+CORES=$(echo $NODEDEF | cut -d' ' -f4 | cut -d '=' -f2)
+THREADS_PER_CORE=$(echo $NODEDEF | cut -d' ' -f5 | cut -d '=' -f2)
+GPUS=$(echo $NODEDEF | cut -d' ' -f7 | cut -d '=' -f2 | cut -d':' -f2)
+CORES_PER_GPUS=$(( CORES/GPUS/THREADS_PER_CORE ))
+
+echo "Running on $CORES cores, $THREADS_PER_CORE threads per core, $GPUS GPUs, $CORES_PER_GPUS cores per GPU."
+
+# --gres-flags=enforce-binding --tres-bind=gres/gpu:closest
+sbatch -N1 -p gpu --gpus-per-node=$GPUS --cpus-per-gpu=$CORES_PER_GPUS --mem=0 $script_dir/text2image_lora.slurm
