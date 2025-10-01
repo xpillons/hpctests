de0c2faf289f10df2dae8bf6fba9404b832dfccc
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-10-01 19:14:45 +0200

    update SBATCH instructions for the container

--- enroot/hello_orcaflex.sh
+++ enroot/hello_orcaflex.sh
@@ -4,8 +4,16 @@
 #SBATCH -o %x_%j.log
 #SBATCH --container-image /shared/containers/orcina+orcaflex+ofx114e-py311.sqsh
 #SBATCH --container-writable
-#SBATCH --container-env=FLEXNET_ADDRESS=licserver.contoso.com
-#SBATCH --export=MELLANOX_VISIBLE_DEVICES=none
+#SBATCH --no-container-entrypoint
+#SBATCH --container-remap-root
+#SBATCH --container-env=FLEXNET_ADDRESS
+#SBATCH --export=MELLANOX_VISIBLE_DEVICES=none,FLEXNET_ADDRESS=foo.com
+
+printenv
+
+echo "running as $(whoami)"
+echo "running /usr/local/bin/entrypoint.sh"
+/usr/local/bin/entrypoint.sh
 
 python --version
 python3 --version
